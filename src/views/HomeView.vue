24edaa43914db37299a6815384e1c8cf6473e399
Korvin <mr.gold1@mail.ru>
2023-06-11 17:16:53 +0300

    initial master

---
+++ src/views/HomeView.vue
@@ -0,0 +1,13 @@
+<template>
+  <main>
+    <h1>Hello world</h1>
+    <h3>{{counterStore.count}}</h3>
+    <button @click="counterStore.increment">+1</button>
+  </main>
+</template>
+
+<script setup>
+import {useCounterStore} from "../stores/counter";
+
+const counterStore = useCounterStore()
+</script>
---
+++ src/views/ProfileView.vue
@@ -0,0 +1,12 @@
+<template>
+  <h1>This is an profile page</h1>
+  <div>this is my description</div>
+  <div>{{ counterStore.count }}</div>
+  <button @click="counterStore.increment">+1</button>
+</template>
+
+<script setup>
+import { useCounterStore } from '../stores/counter'
+
+const counterStore = useCounterStore()
+</script>
