5270a5a2b5f1a09a660e956cb2fdddc5cc00c114
teaMasterAl <bushnev.site@gmail.com>
2023-06-29 20:09:23 +0300

    add dark and light theme add button toggle theme

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,41 @@
 <template>
-  <nav class="menu">
-    <RouterLink class="menu__link" to="/">Home</RouterLink>
-    <RouterLink class="menu__link" to="/profile">Profile</RouterLink>
-  </nav>
-  <RouterView />
+  <div class="theme" :class="themeName">
+    <div>
+      <span>Тема: </span>
+      <button @click="toggleTheme">{{themeButtonText}}</button>
+    </div>
+    <nav class="menu">
+      <RouterLink class="menu__link" to="/">Home</RouterLink>
+      <RouterLink class="menu__link" to="/profile">Profile</RouterLink>
+    </nav>
+    <RouterView />
+  </div>
 </template>
 
 <script setup>
 import { RouterLink, RouterView } from 'vue-router'
-import { onMounted } from 'vue'
+import {computed, onMounted, ref} from 'vue'
 import { useProfileStore } from './stores/profile'
+
+const themeName = ref('light')
+
+const themeButtonText = computed(() => themeName.value === 'light'? 'Тёмная' : 'Светлая')
+const toggleTheme = () => {
+  themeName.value = themeName.value === 'light'? 'dark' : 'light'
+}
 
 const profileStore = useProfileStore()
 
 onMounted(() => {
+  if (window?.matchMedia('(prefers-color-scheme: dark)').matches) {
+    themeName.value = 'dark'
+  }
+
   profileStore.initProfile()
 })
 </script>
 
 <style>
-  body{
-    font-family: "PT Sans", "Arial", sans-serif;
-    margin: 0;
-  }
-  a{
-    text-decoration: none;
-  }
-
   .menu{
     display: flex;
     justify-content: center;
